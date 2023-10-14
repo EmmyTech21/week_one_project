c6dd98c77fa6100dd365f40cf2483db45971c274
EmmyTech21 <128182947+EmmyTech21@users.noreply.github.com>
2023-10-14 09:09:01 -0700

    week one project commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.js'
+import sedan from './images/icon-sedans.svg'
+import luxury from './images/icon-luxury.svg'
+import suvs from './images/icon-suvs.svg'
+export default function App() {
+  return  <div className='container'>
+    <div className='bright'>
+    <NewCard name="SEDANS" photoName={sedan} text="Choose a sedan for
+     its afoordability and excellent fuel economy. Ideal
+      for cruosing in the city or an your next road trip"/>
+</div>
+<div className='dark'>
+    <NewCard  name="SEDANS" photoName={suvs} text="Take on SUV for it's 
+    spacious interior power.and versartility perfect for your next family vacation and off-road adventure"/>
+</div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<div className='verydark'>
+      <NewCard name="SEDANS" photoName={luxury} text="Cruise in the best car brands without 
+      the bloatedprices. Enjoy the enhanced comfort of a luxury rental and arrive in style"/>
+      </div>
+  </div>
 }
 
-export default App;
+
+function NewCard(props){
+  return <div>
+    <img alt={props.key} src={props.photoName}  />
+    <h2>{props.name}</h2>
+    <p>{props.text}</p>
+    <button>Learn More</button>
+  </div>
+}
